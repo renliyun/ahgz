2b53324ffced518852e7e698b6fdca04b6f200c6
renlirong <renliyun@live.cn>
2020-12-15 11:34:03 +0800

    all is new

---
+++ src/test/java/com/vot/ahgz/test/AddressListTest.java
@@ -0,0 +1,16 @@
+package com.vot.ahgz.test;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AddressListTest {
+
+    @Test
+    public void get(){
+
+
+
+    }
+}
